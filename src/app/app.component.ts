08b591a91bec2b2b08c73da7eb6148131882f82c
pelucheasesino <jerryjaras@hotmail.com>
2023-05-28 15:45:50 -0500

    Fin de sección 4

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  public title:string = 'Mi primera app de Angular';
+  /* public counter:number=10;
+
+  increasedBy(value:number):void{
+    if(value)
+    this.counter+=value;
+  }
+
+  resetCounter():void{
+    this.counter=10;
+  } */
+}
